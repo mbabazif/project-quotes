5007e8e8316a6a647c7dafff538239672e119803
Florence <mbababziflorence81@gmail.com>
2019-01-28 14:01:04 +0200

    Adding Author

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,27 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Quote } from '@angular/compiler';
+import { Component, OnInit } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: "app-quote",
+  templateUrl: "./quote.component.html",
+  styleUrls: ["./quote.component.css"]
 })
 export class QuoteComponent implements OnInit {
-
   quotes = [
-    new Quote(1,'Success is not final;failure is not fatal:it is the courage to continue that counts'),
-    new Quote(2,'It is better to fail inoriginality than to succesd in imitation'),
-    new Quote(3, "The road to success and the road to failure are almost exactly the same " ),
-    new Quote(4, 'success usually comes to those who rae too busy to be looking for it ),
-    new Quote(5, 'success is not accident its the result of of pefection,hard work and leaning from failure),
-   
-]
+    new Quote(
+      1,
+      "Success is not final;failure is not fatal:it is the courage to continue that counts","nnnnnnn",new Date(2018, 3, 14)
+    ),
+    new Quote(
+      2,
+      "It is better to fail inoriginality than to succesd in imitation","t8iigftig",new Date(2018, 6, 9)
+    ),
+    new Quote(
+      3,
+      "The road to success and the road to failure are almost exactly the same","ttttttt",new Date(2018, 6, 9)
+    ),
+    new Quote(
+      4,
+      "success usually comes to those who rae too busy to be looking for it","dddddd",new Date(2018, 1, 12)
+    ),
+    new Quote(
+      5,
+      "success is not accident its the result of of pefection,hard work and leaning from failur","ddddddd",new Date(2018, 0, 18)
+    )
+  ];
 
-toogleDetails(index){
-    this.quotes[index].showDescription = !this.quotes[index].showDescription;
-}
+  toogleDetails(index) {
+    this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+  }
 
-constructor() { }
-ngOnInit() {
-}
+  constructor() {}
+  ngOnInit() {}
 }
