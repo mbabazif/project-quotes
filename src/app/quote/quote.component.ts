30c4ce13465bf5df86b99a367d962fbba79d84d9
Florence <mbababziflorence81@gmail.com>
2019-01-28 14:27:31 +0200

    Adding form

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,23 +10,23 @@ export class QuoteComponent implements OnInit {
   quotes = [
     new Quote(
       1,
-      "Success is not final;failure is not fatal:it is the courage to continue that counts","nnnnnnn",new Date(2018, 3, 14)
+      "Success is not final;failure is not fatal:it is the courage to continue that counts","success",new Date(2018, 3, 14)
     ),
     new Quote(
       2,
-      "It is better to fail inoriginality than to succesd in imitation","t8iigftig",new Date(2018, 6, 9)
+      "It is better to fail in originality than to succesd in imitation","imitation",new Date(2018, 6, 9)
     ),
     new Quote(
       3,
-      "The road to success and the road to failure are almost exactly the same","ttttttt",new Date(2018, 6, 9)
+      "The road to success and the road to failure are almost exactly the same","failure",new Date(2018, 6, 9)
     ),
     new Quote(
       4,
-      "success usually comes to those who rae too busy to be looking for it","dddddd",new Date(2018, 1, 12)
+      "success usually comes to those who rae too busy to be looking for it","busy",new Date(2018, 1, 12)
     ),
     new Quote(
       5,
-      "success is not accident its the result of of pefection,hard work and leaning from failur","ddddddd",new Date(2018, 0, 18)
+      "success is not accident its the result of of pefection,hard work and leaning from failur","result",new Date(2018, 0, 18)
     )
   ];
 
