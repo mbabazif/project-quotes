218d2dfae922b4a717f43b430bea8ca2bc69564c
Florence <mbababziflorence81@gmail.com>
2019-01-28 16:29:22 +0200

    adding date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,10 @@
 export class Quote {
+    upvote=0;
+    downvote=0;
     public showdescription:boolean
     constructor(public id:number, public name:string,public description:string,public completeDate:Date){
-        this.showdescription=false
+        this.showdescription=false,
+        this.upvote=0;
+        this.downvote=0;
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,28 +7,70 @@ import { Quote } from "../quote";
   styleUrls: ["./quote.component.css"]
 })
 export class QuoteComponent implements OnInit {
+  upvote = 0;
+  downvote = 0;
   quotes = [
     new Quote(
       1,
-      "Success is not final;failure is not fatal:it is the courage to continue that counts","success",new Date(2018, 3, 14)
+      "Success is not final;failure is not fatal:it is the courage to continue that counts",
+      "success",
+      new Date(2018, 3, 14)
     ),
     new Quote(
       2,
-      "It is better to fail in originality than to succesd in imitation","imitation",new Date(2018, 6, 9)
+      "It is better to fail in originality than to succesd in imitation",
+      "imitation",
+      new Date(2018, 6, 9)
     ),
     new Quote(
       3,
-      "The road to success and the road to failure are almost exactly the same","failure",new Date(2018, 6, 9)
+      "The road to success and the road to failure are almost exactly the same",
+      "failure",
+      new Date(2018, 6, 9)
     ),
     new Quote(
       4,
-      "success usually comes to those who rae too busy to be looking for it","busy",new Date(2018, 1, 12)
+      "success usually comes to those who rae too busy to be looking for it",
+      "busy",
+      new Date(2018, 1, 12)
     ),
     new Quote(
       5,
-      "success is not accident its the result of of pefection,hard work and leaning from failur","result",new Date(2018, 0, 18)
+      "success is not accident its the result of of pefection,hard work and leaning from failur",
+      "result",
+      new Date(2018, 0, 18)
     )
   ];
+
+  //   scope.isUpVoted = false;
+  //     this.watch("isUpVoted",(newVal, oldVal){
+  //     if(newVal != oldVal){
+  //         if(newVal){
+  //            // first click
+  //            // upvote
+
+  //         }else{
+  //            // second click
+  //            // remove upvote
+  //         }
+
+  //         }
+  // });
+
+  // .isDownVoted = false;
+  //         this.$watch("isDownVoted",(newVal, oldVal){
+  //         if(newVal != oldVal){
+  //             if(newVal){
+  //                // first click
+  //                // downvote
+
+  //             }else{
+  //                // second click
+  //                // remove downvote
+  //             }
+
+  //             }
+  //     });
 
   toogleDetails(index) {
     this.quotes[index].showdescription = !this.quotes[index].showdescription;
